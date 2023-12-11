26ff23bbcb5fbd86753358bd64fab1efdeb3c6e0
taqsblaze <tanakah30@gmail.com>
2023-12-11 16:25:17 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="tanakah30@gmail.com",
     long_description="""
 ### Live-Host
-### Live-Host a robust module for checking is a host is live
+### Live-Host a robust module for checking if a host is live
 
 - This module checks if a target host is alive by sending a get request to the host.
 
